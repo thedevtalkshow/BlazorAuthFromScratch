2b04dbbd4bd00b159059edd8344ff3349fbbe4dc
Chris Gomez <cagomez@cellbycell.com>
2024-03-29 00:34:37 -0400

    Added authentication to app and to home page An EditForm on the home page "logs in users" per the Submit method.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddRazorComponents();
 builder.Services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
 
 builder.Services.AddAuthorization();
+builder.Services.AddAuthentication();
 
 var app = builder.Build();
 
